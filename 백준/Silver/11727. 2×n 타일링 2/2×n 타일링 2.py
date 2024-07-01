7d41644170cc094fc1bbc6ae7e47fa76c960b077
seo0o519 <135585375+seo0o519@users.noreply.github.com>
2024-07-02 01:32:45 +0900

    [Silver III] Title: 2×n 타일링 2, Time: 36 ms, Memory: 31312 KB -BaekjoonHub

--- 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.py
+++ 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.py
@@ -5,16 +5,13 @@ def rec(n):
     # base case
     if n==1 :
         memo[n] = 1
-        return memo[n]
     elif n==2 :
         memo[n] = 3
-        return memo[n]
     
     # 이전에 계산된 값이 있는 경우
-    elif memo[n] != 0 :
-        return memo[n] 
-    else:
+    elif memo[n] == 0 :
         memo[n] = rec(n-1) + rec(n-2) * 2 
-        return memo[n]
+
+    return memo[n]
 
 print(rec(N)%10007)
