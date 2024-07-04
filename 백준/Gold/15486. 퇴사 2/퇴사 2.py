fab04a56017dc0ec176387498b71964cd60233b3
seo0o519 <135585375+seo0o519@users.noreply.github.com>
2024-07-04 23:18:58 +0900

    Update 퇴사 2.py

--- 백준/Gold/15486. 퇴사 2/퇴사 2.py
+++ 백준/Gold/15486. 퇴사 2/퇴사 2.py
@@ -1,18 +1,13 @@
 import sys
-
 input = lambda: sys.stdin.readline().rstrip()
-
 n = int(input())
-t, p = [0 for _ in range(n + 1)], [0 for _ in range(n + 1)]
-for i in range(1, n + 1):
-    t[i], p[i] = map(int, input().split())
+t_p = [list(map(int, input().split())) for _ in range(n)]
+t_p.insert(0, [0,0]) # 인덱스랑 날짜 일치시키기 위함
+memo = [0 for _ in range(n+1)]
 
-dp = [0 for _ in range(n + 1)]
-
-for i in range(1, n + 1):
-    dp[i] = max(dp[i], dp[i - 1])  # 이전까지의 최댓값
-    fin_date = i + t[i] - 1  # 당일 포함
-    if fin_date <= n:  # 최종일 안에 일이 끝나는 경우
-        # i일부터는 일을 해야하므로 i일에 얻을 수 있는 최댓값이 아닌 i-1일까지 얻을 수 있는 최댓값을 구한다
-        dp[fin_date] = max(dp[fin_date], dp[i - 1] + p[i])
-print(max(dp))
+for i in range(1,n+1):
+    memo[i] = max(memo[i-1], memo[i])
+    fin_date = i + t_p[i][0] - 1
+    if fin_date < n+1 :
+        memo[fin_date] = max(memo[fin_date], memo[i-1] + t_p[i][1])
+print(memo[n])
