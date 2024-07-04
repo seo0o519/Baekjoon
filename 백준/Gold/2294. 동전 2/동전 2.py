7994f914481e12cd10b7013df8401f9c404c858b
seo0o519 <135585375+seo0o519@users.noreply.github.com>
2024-07-04 16:03:25 +0900

    [Gold V] Title: 동전 2, Time: 776 ms, Memory: 44948 KB -BaekjoonHub

---
+++ 백준/Gold/2294. 동전 2/동전 2.py
@@ -0,0 +1,22 @@
+import sys
+sys.setrecursionlimit(10**6)
+
+n, k = map(int, input().split())
+coin = [int(input()) for _ in range(n)]
+memo = [-1 for _ in range(k+1)]
+
+def rec(n):
+    if n == 0 :
+        return 0
+    elif n < 0 :
+        return k
+    
+    if memo[n] == -1 :
+        memo[n] = min(rec(n-c) for c in coin) + 1
+    return memo[n]
+
+ans =  rec(k) 
+if ans > k :
+    print(-1)
+else :
+    print(ans)
