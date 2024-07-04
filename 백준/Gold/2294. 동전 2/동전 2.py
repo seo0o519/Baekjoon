6f9ec077dabe8eac4f91ecad10c706780c189656
seo0o519 <135585375+seo0o519@users.noreply.github.com>
2024-07-04 16:49:03 +0900

    Update 동전 2.py

--- 백준/Gold/2294. 동전 2/동전 2.py
+++ 백준/Gold/2294. 동전 2/동전 2.py
@@ -4,13 +4,11 @@ sys.setrecursionlimit(10**6)
 n, k = map(int, input().split())
 coin = [int(input()) for _ in range(n)]
 memo = [-1 for _ in range(k+1)]
+memo[0] = 0
 
 def rec(n):
-    if n == 0 :
-        return 0
-    elif n < 0 :
+    if n < 0 :
         return k
-    
     if memo[n] == -1 :
         memo[n] = min(rec(n-c) for c in coin) + 1
     return memo[n]
