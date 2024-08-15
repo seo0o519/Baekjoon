127231ea9fd6d979bd8095e2b83f7ec4ba24b796
seo0o519 <135585375+seo0o519@users.noreply.github.com>
2024-08-15 14:56:53 +0900

    [Silver IV] Title: 스택, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/10828. 스택/스택.py
@@ -0,0 +1,43 @@
+import sys
+input=sys.stdin.readline
+
+stack = []
+
+def push(x):
+    stack.append(x)
+
+def pop():
+    if len(stack) == 0:
+        print(-1)
+    else:
+        print(stack.pop())
+
+def size():
+    print(len(stack))
+
+def empty():
+    if len(stack) == 0:
+        print(1)
+    else:
+        print(0)
+
+def top():
+    if len(stack) == 0:
+        print(-1)
+    else:
+        print(stack[-1])
+
+N = int(input())
+for _ in range(N):
+    command = input().split()
+
+    if command[0] == "push":
+        push(int(command[1]))
+    elif command[0] == "pop":
+        pop()
+    elif command[0] == "size":
+        size()
+    elif command[0] == "empty":
+        empty()
+    elif command[0] == "top":
+        top()
