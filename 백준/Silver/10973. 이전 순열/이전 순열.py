20f29ba3f9a09c212992a7d51cf42c289ef07365
Seoyoung Kang <135585375+seo0o519@users.noreply.github.com>
2024-12-17 14:33:37 +0900

    [Silver III] Title: 이전 순열, Time: 44 ms, Memory: 33432 KB -BaekjoonHub

---
+++ 백준/Silver/10973. 이전 순열/이전 순열.py
@@ -0,0 +1,31 @@
+N = int(input())
+perm = list(map(int, input().split()))
+
+x = 0
+y = 0
+
+# 순열 맨 뒤에서부터 앞 > 뒤 가 되면 정지
+for i in range(N-1, 0, -1):
+    if perm[i-1] > perm[i]:
+        x = i-1
+        y = i
+        # 순열 맨 뒤에서부터 x > 뒤 면 swap
+        for j in range(N-1, 0, -1):
+            if perm[x] > perm[j]:
+                temp = perm[x]
+                perm[x] = perm[j]
+                perm[j] = temp
+                break
+        break
+    else:
+        continue
+
+# y인덱스부터 끝까지 뒤집음
+front = perm[0:x+1]
+end = perm[y:][::-1]
+
+if x==0 and y==0:
+    print(-1)
+else:
+    for i in (front+end):
+        print(i, end=" ")
